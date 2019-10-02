119810d3bf1de5a4dcc0b47c86817c7dfeebf9a4
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-10-02 16:10:51 +0200

    added google maps package

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(props){
     super(props);
+
+    this.state = { clicked: false }
   }
 
 
---
+++ src/components/googlemap_react.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const googleMap = (props) => {
+  return (
+    <div className="map-container">
+    </div>
+    );
+
+}
+
+export default googleMap;
